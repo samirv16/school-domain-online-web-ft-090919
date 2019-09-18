8d67a1032842ba319cec3305d9ec5d806102b927
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-18 15:20:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,17 @@
 # code here!
 
 class School
-attr_reader :school 
+attr_reader :school, :roster
 
 def iniatialize=(name)
   @school = name
+  @roster = {}
+end 
 
+def add_student(name, grade)
+    if !@roster.has_key?(grade)
+    @roster[grade] = []
+    end
+  @roster[grade] << name
+  end
 end
